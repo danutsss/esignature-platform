52fd99edb29d8cfca19f129593de5b016f2de5cd
Matei Nicolae - Daniel <matei1337@icloud.com>
2021-11-25 17:41:20 +0200

    eSignature UCRM Platform.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# esignature-platform
-This plugin helps the UCRM clients to sign a digital contract.
+# eSignature UCRM Platform
+
+## How it works?
+
+This plugin handles 2 options: read & sign a digital contract. The information from the contract is pulled from the UCRM Platform through API.
+
+Using Mobile Detect Library I managed to check if the client is using a phone or a tablet and if they are, the contract will automatically download, because I saw that the PDF Viewer is not loading properly when using a phone.
+
+## Configuration
+
+If you want to edit the content of the contract for both options (read & sign), you will need to open `read.php` & `sign.php` and modify the `$HTML` variable.
+
+## Future updates
+
+* Configure the contract content from the plugin's configuration page.
---
+++ src/public.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+// references to Ubnt, ...
+use Ubnt\UcrmPluginSdk\Data\UcrmUser;
+use Ubnt\UcrmPluginSdk\Service\UcrmSecurity;
+use Ubnt\UcrmPluginSdk\Service\UcrmOptionsManager;
+
+use App\Service\TemplateRenderer;
+
+chdir(__DIR__);
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/main.php';
+
+$security = UcrmSecurity::create();
+$user = $security -> getUser();
+
+// Check if user is client, if not send an error message.
+if(!$user -> isClient) {
+    \App\Http::forbidden();
+}
+
+// Render form
+$optionsManager = UcrmOptionsManager::create();
+$renderer = new TemplateRenderer();
+
+// Set the default template and data.
+$template = "overview.php";
+$data = [   'ucrmPublicUrl' => $optionsManager -> loadOptions() -> ucrmPublicUrl    ];
+
+if(isset($_GET["hook"])) {
+    // Handle the possible "hook" parameters...
+    switch($_GET["hook"]) {
+        case "read":
+            $template = "read.php";
+            break;
+
+        case "sign":
+            $template = "sign.php";
+            break;
+
+        default:
+            // Maybe die here or use above default?!
+            die("Unsupported 'hook' parameter.");
+    }
+}
+
+$renderer -> render(__DIR__ . "/views/$template", $data);
